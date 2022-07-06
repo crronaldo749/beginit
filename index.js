db769d4a789965c15187cc49644a779afa457079
crronaldo749 <108025404+crronaldo749@users.noreply.github.com>
2022-07-07 02:15:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function greet(name, lastname, age){
+    document.write(`Hello ${name} ${lastname}. I am ${age} years old.` );
+}
+
+greet('Ankit','Sharma',26);
+
+// calculating square
+function square (number) {
+    return number*number;
+}
+document.write('<br>'+square(3));
